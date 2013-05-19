2d00455d8acae5c157867d3320503538f5814a5c
Donald Stufft <donald@stufft.io>
2013-05-18 22:51:36 -0400

    initial commit

---
+++ pypi_show_urls/__main__.py
@@ -0,0 +1,99 @@
+# Copyright 2013 Donald Stufft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import itertools
+import sys
+import urlparse
+
+import lxml.html
+import requests
+
+from pkg_resources import safe_name
+from setuptools.package_index import distros_for_url
+
+
+def installable(project, url):
+    normalized = safe_name(project).lower()
+    return bool([dist for dist in distros_for_url(url) if
+                        safe_name(dist.project_name).lower() == normalized])
+
+
+def process_page(html, package, url):
+    header = False
+    for link in html.xpath("//a"):
+        try:
+            link.make_links_absolute(url)
+        except ValueError:
+            continue
+
+        if "href" in link.attrib and installable(package, link.attrib["href"]):
+            if not header:
+                print("")
+                print("  Candidates from %s" % url)
+                print("  ----------------" + ("-" * len(url)))
+                header = True
+
+            print("    " + link.attrib["href"])
+
+
+def main():
+    # A list of packages to look for
+    packages = sys.argv[1:]
+
+    session = requests.session()
+    session.verify = False
+
+    for package in packages:
+        print("Download candidates for %s" % package)
+        print("========================" + ("=" * len(package)))
+
+        # Grab the page from PyPI
+        url = "https://pypi.python.org/simple/%s/" % package
+        resp = session.get(url)
+        resp.raise_for_status()
+
+        html = lxml.html.document_fromstring(resp.content)
+
+        spider = set()
+
+        for link in itertools.chain(
+                            html.find_rel_links("download"),
+                            html.find_rel_links("homepage")):
+            try:
+                link.make_links_absolute(url)
+            except ValueError:
+                continue
+
+            if "href" in link.attrib and not \
+                                    installable(package, link.attrib["href"]):
+                parsed = urlparse.urlparse(link.attrib["href"])
+                if parsed.scheme.lower() in ["http", "https"]:
+                    spider.add(link.attrib["href"])
+
+        # Find installable links from the PyPI page
+        process_page(html, package, url)
+
+        # Find installable links from pages we spider
+        for link in spider:
+            try:
+                resp = session.get(link)
+                resp.raise_for_status()
+            except Exception:
+                continue
+
+            html = lxml.html.document_fromstring(resp.content)
+            process_page(html, package, link)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="pypi-show-urls",
+    version="1.0",
+
+    description="Shows all the installation candidates for a list of packages",
+    url="https://github.com/dstufft/pypi-show-urls",
+
+    author="Donald Stufft",
+    author_email="donald@stufft.io",
+
+    install_requires=[
+        "requests",
+        "lxml",
+    ],
+
+    packages=find_packages(exclude=["tests"]),
+
+    entry_points={
+        "console_scripts": [
+            "pypi-show-urls = pypi_show_urls.__main__:main",
+        ],
+    },
+)
