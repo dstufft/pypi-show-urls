9e43fb6fa6f80f4dc9d096868adbc5371b91c46a
Donald Stufft <donald@stufft.io>
2013-05-19 00:05:22 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pypi-show-urls",
-    version="1.3.1",
+    version="1.3.2",
 
     description="Shows all the installation candidates for a list of packages",
     long_description=open("README.rst").read(),
